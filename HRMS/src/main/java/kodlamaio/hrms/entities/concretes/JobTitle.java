9695b6332e6f848e3719469cc5bdf50461d658fa
Ece yaren Taykutgül <ece_belis@hotmail.com>
2021-05-26 22:19:52 +0300

    First Commit

---
+++ HRMS/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -0,0 +1,48 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="job_titles")
+public class JobTitle {
+	
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	@Column(name = "title")
+	private String title;
+	
+	public JobTitle(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	
+	public JobTitle() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+
+}
